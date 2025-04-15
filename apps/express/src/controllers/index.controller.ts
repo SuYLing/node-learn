f18644f24173c401796bb27874ce3b685d5398a6
yuling <sakuriget@gmail.com>
2025-04-15 21:21:41 +0800

    test ejs view  plugin

--- apps/express/src/controllers/index.controller.ts
+++ apps/express/src/controllers/index.controller.ts
@@ -1,5 +1,17 @@
 import type { RequestHandler } from "express"
 
 export const helloExpress: RequestHandler = (req, res, next) => {
-  res.send("hello express!")
+  res.render("index", {
+    title: " home page",
+    people: [
+      {
+        name: "yuling",
+        age: 12,
+      },
+      {
+        name: "sakurige",
+        age: 18,
+      },
+    ],
+  })
 }
--- apps/express/src/index.ts
+++ apps/express/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express"
+import path from "path"
 import "./config/env"
 import "./databases/mongo"
 import { connectToMongo } from "./databases/mongo"
 import router from "./routes"
-
 // get some variables from env
 const PORT = process.env.PORT
 
 // create a root application
 const app = express()
+
+// set view engine
+app.set("view engine", "ejs")
+app.set("views", path.join(import.meta.dirname, "views"))
 
 // use middleware
 app.use(express.json())
