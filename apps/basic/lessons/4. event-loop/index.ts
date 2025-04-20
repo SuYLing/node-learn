96cbf4ae5a2ac3d61047c83d7a9d4b7d10d4e29d
yuling <sakuriget@gmail.com>
2025-04-20 20:47:47 +0800

    feat: event loop

---
+++ apps/basic/lessons/4. event-loop/index.ts
@@ -0,0 +1,38 @@
+// main > micro > macro
+// mirco
+//    1. Promise.resolve
+//    2. process.nextTick
+// macro
+//    1. setTimeout
+//    2. setImmediate
+//    3. setInterval
+import fs from "fs"
+import path from "path"
+fs.readFile(
+  path.resolve(process.cwd(), "../3. http-module/index.ts"),
+  (err, data) => {
+    console.log(data.toString())
+  }
+)
+console.log("1: start")
+
+setTimeout(() => {
+  console.log("2: time out 0s")
+}, 0)
+
+setImmediate(() => {
+  console.log("3: setImmediate")
+})
+Promise.resolve().then(() => {
+  console.log("4: promise resolve callback")
+})
+process.nextTick(() => {
+  console.log("5: process nexttick")
+})
+console.log("6: end")
+// 1: start
+// 6: end
+// 4: promise resolve callback
+// 5: process nexttick
+// 3: setImmediate
+// 2: time out 0s
