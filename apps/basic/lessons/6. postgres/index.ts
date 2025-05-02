e9f225f9d5851c44af9af9039d3f2e54c9b12319
yuling <sakuriget@gmail.com>
2025-05-02 21:14:28 +0800

    feat: test postgress quuery

--- apps/basic/lessons/6. postgres/index.ts
+++ apps/basic/lessons/6. postgres/index.ts
@@ -1,7 +1,21 @@
-import { Pool } from "pg"
+import { Pool, type QueryArrayConfig } from "pg"
 const pool = new Pool({
+  // postgress => :// => user => password => host => posr => database
   connectionString: "postgress://postgres:123yuling@localhost:5432",
-})
+}) // 创建连接
 pool.connect().then(() => {
   console.log("connected to postgress")
 })
+
+export async function query(text: string, params: QueryArrayConfig) {
+  const start = Date.now()
+  try {
+    const result = await pool.query(text, params)
+    const duration = Date.now() - start
+
+    console.log("Excyted query: ", text, duration, result.rowCount?.toString())
+    console.log()
+  } catch (error) {
+    console.log(error)
+  }
+}
